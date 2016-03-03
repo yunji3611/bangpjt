18b1938e533409feb4434996e5f8cf67aed2b414
YUNJI <a9a8a7a@naver.com>
2016-03-03 18:36:19 +0900

    mypage form변경

--- routes/mypages.js
+++ routes/mypages.js
@@ -6,12 +6,12 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     if (req.secure) {
         res.json({
-            "message": "프로필이 조회되었습니다...",
-            "date": {
-                "name": "홍길동",
-                "photo_url": "https://www.google.co.kr/imgres?imgurl=http://file2.instiz.net/data/file2/2016/01/10/3/7/9/379f6ddda72b634b23a567a4b2217103.jpg&imgrefurl=http://www.instiz.net/pt/1323326&h=581&w=513&tbnid=NFLPI_2vxt_BAM:&docid=PyD6eyEzexOeeM&ei=oDLMVtvwJOezmwWtx6iIDw&tbm=isch&ved=0ahUKEwjbwIz81I3LAhXn2aYKHa0jCvEQMwgfKAQwBA",
-                "myscrap_count": 5,
-                "mypost_count": 10
+            "result": {
+                "message": "프로필이 조회되었습니다...",
+                "mypageData": {
+                    "name": "홍길동",
+                    "photo_path": "hong.jpg"
+                }
             }
         });
     } else {
