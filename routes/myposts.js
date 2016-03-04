fbb7cdb658a1116fe41cec4c5a19eaa893e31d56
YUNJI <a9a8a7a@naver.com>
2016-03-04 17:34:57 +0900

    ..

--- routes/myposts.js
+++ routes/myposts.js
@@ -5,24 +5,28 @@ var router = express.Router();
 // 내가 올린 게시물 목록 조회
 router.get('/', function (req, res, next) {
 
-        res.json({
-            "result": {
-                "message": "내가쓴 글이 조회되었습니다...",
-                "postData": {
-                    "mypost_count": 20,
-                    "page": 1,
-                    "listPerPage": 2,
-                    "list": [{"myposts_id": 1111,
+    res.json({
+        "result": {
+            "message": "내가쓴 글이 조회되었습니다...",
+            "postData": {
+                "mypost_count": 20,
+                "page": 1,
+                "listPerPage": 2,
+                "postList": [{
+                        "post_id": 1111,
                         "file_url": "12dsa",
                         "photo_url": "###",
-                        "hashtag":"침대"},
-                        {"post_id": 2,
-                            "file_url": "12dsa",
-                            "photo_url": "###",
-                            "hashtag":"의자"}]
-                }
+                        "hash_tag": "침대"
+                },
+                    {
+                        "post_id": 2,
+                        "file_url": "12dsa",
+                        "photo_url": "###",
+                        "hash_tag": "의자"
+                    }]
             }
-        });
+        }
+    });
 
 });
 
