134ef0e02b82e88d785eb46b8a4983a9d03e95bd
minju <minju236@naver.com>
2016-03-04 16:14:34 +0900

    게시물 목록 조회

--- routes/posts.js
+++ routes/posts.js
@@ -2,35 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function (req, res, next) {
-    var result = {
-        "result": {
-            "message": "게시물 목록이 조회되었습니다.",
-            "data": {
-                "count": 20,
-                "page": 1,
-                "listperPage": 6
-            },
-            "list": [{
-                "id": 1111,
-                "photo_url": "./public/photos/user/profile.jpg",
-                "interior_url": "./public/photos/interior/europe.jpg",
-                "scrap_count": 22,
-                "hashtag": ["의자", "침대"],
-                "category": "북유럽",
-                "post_count": 50,
-                "detail": [{
-                    "furniture_url": "./public/photos/furniture/chair.jpg",
-                    "furniture": ["IKEA", 12000, "20*40", "link", "color"],
-                    "content": "게시물 내용",
-                    "reply": "게시물 댓글"
-                }]
-            }]
-        }
-    }
-    res.json(result);
-})
-
 
 router.post('/', function (req, res, next) {
     var result = {
