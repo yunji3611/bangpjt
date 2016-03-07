e0a94b668274f2d6399a1a8a8683fb5fcb07a3c2
YUNJI <a9a8a7a@naver.com>
2016-03-07 19:19:57 +0900

    취향조사 목록

--- routes/preferences.js
+++ routes/preferences.js
@@ -7,14 +7,55 @@ router.get('/', function (req, res, next) {
     res.json({
         "result": {
             "message": "취향조사 내용이 전달되었습니다..",
-            "preferData": [{"questionary_id": 1,
-                "questionary": "당신의 나이대는?",
-                "item_seq": 1,
-                "item": "20"},
-                {"questionary_id": 1,
-                    "questionary": "어떤 스타일을 좋아해요?",
+            "preferData": [
+                {
+                    "questionary_id": 1,
+                    "questionary": "당신의 연령대는?",
+                    "item_seq": 1,
+                    "item": "20"
+                },
+                {
+                    "questionary_id": 1,
+                    "questionary": "당신의 연령대는?",
                     "item_seq": 2,
-                    "item": "도시적인 스타일"}]
+                    "item": "30"
+                },
+                {
+                    "questionary_id": 1,
+                    "questionary": "당신의 연령대는?",
+                    "item_seq": 3,
+                    "item": "40"
+                },
+                {
+                    "questionary_id": 1,
+                    "questionary": "당신의 연령대는?",
+                    "item_seq": 4,
+                    "item": "50"
+                },
+                {
+                    "questionary_id": 2,
+                    "questionary": "당신의 취향은?",
+                    "item_seq": 1,
+                    "item": "도시적이고 간결한 블랙&화이트"
+                },
+                {
+                    "questionary_id": 2,
+                    "questionary": "당신의 취향은?",
+                    "item_seq": 2,
+                    "item": "전원적이고 아기자기한 꽃내음"
+                },
+                {
+                    "questionary_id": 2,
+                    "questionary": "당신의 취향은?",
+                    "item_seq": 3,
+                    "item": "네츄럴한 우드스타일의 실용주의"
+                },
+                {
+                    "questionary_id": 2,
+                    "questionary": "당신의 취향은?",
+                    "item_seq": 4,
+                    "item": "아날로그감성의 빛바랜"
+                }]
         }
 
     })
