1a09429b15d808746573aa9ec02e65b9c6a206fa
YUNJI <a9a8a7a@naver.com>
2016-03-08 09:33:13 +0900

    취향조사 목록수정

--- routes/preferences.js
+++ routes/preferences.js
@@ -11,51 +11,28 @@ router.get('/', function (req, res, next) {
                 {
                     "questionary_id": 1,
                     "questionary": "당신의 연령대는?",
-                    "item_seq": 1,
-                    "item": "20"
-                },
-                {
-                    "questionary_id": 1,
-                    "questionary": "당신의 연령대는?",
-                    "item_seq": 2,
-                    "item": "30"
-                },
-                {
-                    "questionary_id": 1,
-                    "questionary": "당신의 연령대는?",
-                    "item_seq": 3,
-                    "item": "40"
-                },
-                {
-                    "questionary_id": 1,
-                    "questionary": "당신의 연령대는?",
-                    "item_seq": 4,
-                    "item": "50"
+                    "item": [{"seq": 1, "item": 20},
+                            {"seq": 2, "item": 30},
+                            {"seq": 3, "item": 40},
+                            {"seq": 4, "item": 50}]
                 },
                 {
                     "questionary_id": 2,
                     "questionary": "당신의 취향은?",
-                    "item_seq": 1,
-                    "item": "도시적이고 간결한 블랙&화이트"
+                    "item": [{"seq": 1, "item": "도시적이고 간결한 블랙&화이트"},
+                             {"seq": 2, "item": "전원적이고 아기자기한 꽃내음"},
+                             {"seq": 3, "item": "아날로그감성의 빛바랜"},
+                             {"seq": 4, "item": "아날로그감성의 빛바랜"}]
                 },
                 {
-                    "questionary_id": 2,
-                    "questionary": "당신의 취향은?",
-                    "item_seq": 2,
-                    "item": "전원적이고 아기자기한 꽃내음"
-                },
-                {
-                    "questionary_id": 2,
-                    "questionary": "당신의 취향은?",
-                    "item_seq": 3,
-                    "item": "네츄럴한 우드스타일의 실용주의"
-                },
-                {
-                    "questionary_id": 2,
-                    "questionary": "당신의 취향은?",
-                    "item_seq": 4,
-                    "item": "아날로그감성의 빛바랜"
-                }]
+                    "questionary_id": 3,
+                    "questionary": "당신이 인테리어 할 때 가장 중요시하는 소품은?",
+                    "item": [{"seq": 1, "item": "조명"},
+                            {"seq": 2, "item": "조명"},
+                            {"seq": 3, "item": "탁자"},
+                            {"seq": 4, "item": "커버류"}]
+                }
+            ]
         }
 
     })
